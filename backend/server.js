32de78d760379782e978f53a2c03326225aa526f
harshithbavirishetty <harshithbavirishetty@gmail.com>
2025-04-27 15:19:48 +0530

    Description of the changes made

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,19 @@
 const express = require('express');
-const cors = require('cors');  // <-- ADD this
+const cors = require('cors');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/auth');
+const postRoutes = require('./routes/post');  // Import the post routes
 require('dotenv').config();
 
 const app = express();
 
-app.use(cors()); // <-- ADD this line
-
+app.use(cors());
 app.use(express.json());
 
 connectDB();
 
 app.use('/api', authRoutes);
+app.use('/api/posts', postRoutes);  // Use the post routes here
 
 app.get('/', (req, res) => {
   res.send('API is running...');
